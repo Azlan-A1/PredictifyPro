7bf189374e2d20485829c3a513f9e25e67ca50c1
derektan12 <derektan101@gmail.com>
2024-04-28 03:02:01 -0400

    second commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <nav className="border-2 border-red-500 flexBetween max-container
      padding-container relative z-30 py-5">
         <Link href="/">
+          <Image src=""
             
         </Link>
     </nav>
